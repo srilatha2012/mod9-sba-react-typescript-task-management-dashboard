4d4384727dbd3a9530607138e9bcdedd353368cc
srilatha2012 <srilathp2012@gmail.com>
2026-04-28 18:36:10 -0400

    Implemented status update and Delete functionality

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,6 +1,6 @@
-import type { TaskItemProps } from "../../types";
+import type { TaskItemProps, TaskStatus } from "../../types";
 
-export function TaskItem({ task }: TaskItemProps) {
+export function TaskItem({ task, onStatusChange, onDeleteTask }: TaskItemProps) {
 
     return (
         <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm hover:shadow-md transition">
@@ -10,6 +10,24 @@ export function TaskItem({ task }: TaskItemProps) {
                 <p><span className="font-semibold"> {task.status}</span></p>
                 <p><span className="font-semibold">{task.priority}</span></p>
                 <p><span className="font-semibold">{task.dueDate}</span></p>
+                <select
+                    value={task.status}
+                    onChange={(e) =>
+                        onStatusChange(task.id, e.target.value as TaskStatus)
+                    }
+                    className="mt-2 rounde"
+                >
+                    <option value="pending">Pending</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                </select>
+                <button
+                    onClick={() => onDeleteTask(task.id)
+                    }
+                    className="mt-3 rounded bg-red-500 px-3 py-1 text-white"
+                >
+                Delete
+                </button>
             </div>
         </div>
 
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,12 +1,17 @@
 import type { TaskListProps } from "../../types";
 import { TaskItem } from "./TaskItem";
 
-export function TaskList({ tasks }: TaskListProps) {
+export function TaskList({ tasks,onStatusChange, onDeleteTask }: TaskListProps) {
 
     return (
         <div className="space-y-4">
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
+                <TaskItem 
+                key={task.id} 
+                task={task} 
+                onStatusChange={onStatusChange}
+                onDeleteTask ={onDeleteTask}
+                />
             ))}
         </div>
     )
